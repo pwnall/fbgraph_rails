a996e9d6f8b61a52ae77ee20759f6d0d991d20dc
Victor Costan <costan@gmail.com>
2010-10-15 20:40:14 -0400

    Facebook login button helper.

--- app/helpers/facebook_oauth_helper.rb
+++ app/helpers/facebook_oauth_helper.rb
@@ -1,4 +1,10 @@
 module FacebookOauthHelper
   attr_reader :facebook_client
-  attr_reader :current_facebook_access_token  
+  attr_reader :current_facebook_access_token
+  
+  def facebook_login_form
+    form_tag new_facebook_oauth_path, :method => 'get' do
+      image_submit_tag 'facebook_connect.gif', :alt => 'Connect with Facebook'
+    end
+  end
 end
